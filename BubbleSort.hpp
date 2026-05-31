d764d5a90cdbd152fa0d629f2fa7a6ed4c38f9c7
ehabda05 <habda.escander@gmail.com>
2026-05-31 18:02:38 -0400

    Cleaner Bubble and Insertion Sort Format

--- BubbleSort.hpp
+++ BubbleSort.hpp
@@ -1,22 +1,30 @@
+#ifndef BUBBLESORT_HPP
+#define BUBBLESORT_HPP
+
 #include <cstddef>
 #include <utility>
 
 class BubbleSort {
   public:
     template<typename T>
-    void sort(T& nums) {
-        std::size_t n = nums.size();
-        bool isSorted = false;
-
-        while (!isSorted && n > 1) {
-            isSorted = true;
-            for (std::size_t i = 1; i < n; i++) {
-                if (nums[i] < nums[i-1]) {
-                    isSorted = false;
-                    std::swap(nums[i],nums[i-1]);
-                }
-            }
-            n--;
-        }
-    }
+    void sort(T& nums);
 };
+
+template<typename T>
+void BubbleSort::sort(T& nums) {
+    std::size_t n = nums.size();
+    bool isSorted = false;
+
+    while (!isSorted && n > 1) {
+        isSorted = true;
+        for (std::size_t i = 1; i < n; i++) {
+            if (nums[i] < nums[i-1]) {
+                isSorted = false;
+                std::swap(nums[i],nums[i-1]);
+            }
+        }
+        n--;
+    }
+}
+
+#endif // BUBBLESORT_HPP
--- InsertionSort.hpp
+++ InsertionSort.hpp
@@ -1,19 +1,27 @@
+#ifndef INSERTIONSORT_HPP
+#define INSERTIONSORT_HPP
+
 #include <cstddef>
 
 class InsertionSort {
   public:
     template<typename T>
-    void sort(T& nums) {
-        std::size_t n = nums.size();
-        
-        for (std::size_t i = 1; i < n; i++) {
-            auto key = nums[i];
-            std::size_t j = i;
-            while (j > 0 && nums[j-1] > key) {
-                nums[j] = nums[j-1];
-                j--;
-            }
-            nums[j] = key;
-        }
-    }
+    void sort(T& nums);
 };
+
+template<typename T>
+void InsertionSort::sort(T& nums) {
+    std::size_t n = nums.size();
+    
+    for (std::size_t i = 1; i < n; i++) {
+        auto key = nums[i];
+        std::size_t j = i;
+        while (j > 0 && nums[j-1] > key) {
+            nums[j] = nums[j-1];
+            j--;
+        }
+        nums[j] = key;
+    }
+}
+
+#endif // INSERTIONSORT_HPP
