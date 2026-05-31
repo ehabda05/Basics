aad31c5fcd29dd669e8c4a7f8a811df86e84b74f
ehabda05 <habda.escander@gmail.com>
2026-05-31 17:52:53 -0400

    Cleaner Queue and Deque Format

--- Deque.hpp
+++ Deque.hpp
@@ -1,103 +1,24 @@
+#ifndef DEQUE_HPP
+#define DEQUE_HPP
+
 #include <cstddef>
 #include <stdexcept>
 
 template<typename T>
 class Deque {
   public:
-    Deque() : first(nullptr), last(nullptr),sz(0) {}
-
-    ~Deque() {
-        clear();
-    }
-
-    Deque(const Deque& other) : first(nullptr), last(nullptr), sz(0) {
-        Node* cur = other.first;
-        while (cur != nullptr) {
-            push_back(cur->val);
-            cur = cur->next;
-        }
-    }
-
-    Deque& operator=(const Deque& other) {
-        if (this == &other) return *this;
-
-        clear();
-        Node* cur = other.first;
-        while (cur != nullptr) {
-            push_back(cur->val);
-            cur = cur->next;
-        }
-
-        return *this;
-    }
-
-    const T& front() const {
-        if (empty()) {
-            throw std::out_of_range("Deque empty\n");
-        }
-        return first->val;
-    }
-
-    const T& back() const {
-        if (empty()) {
-            throw std::out_of_range("Deque empty\n");
-        }
-        return last->val;
-    }
-
-    void push_front(const T& value) {
-        Node* newNode = new Node(value);
-        if (first == nullptr) {
-            first = newNode;
-            last = newNode;
-        } else {
-            first->prev = newNode;
-            newNode->next = first;
-            first = newNode;
-        }
-        sz++;
-    }
-
-    void push_back(const T& value) {
-        Node* newNode = new Node(value);
-        if (first == nullptr) {
-            first = newNode;
-            last = newNode;
-        } else {
-            newNode->prev = last;
-            last->next = newNode;
-            last = last->next;
-        }
-        sz++;
-    }
-
-    void pop_front() {
-        if (empty()) return;
-        Node* oldFirst = first;
-        first = first->next;
-        if (first != nullptr) first->prev = nullptr;
-        delete oldFirst;
-        sz--;
-        if (sz == 0) last = nullptr;
-    }
-
-    void pop_back() {
-        if (empty()) return;
-        Node* oldLast = last;
-        last = last->prev;
-        if (last != nullptr) last->next = nullptr;
-        delete oldLast;
-        sz--;
-        if (sz == 0) first = nullptr;
-    }
-
-    bool empty() const {
-        return (sz == 0);
-    }
-
-    void clear() {
-        while (!empty()) pop_front();
-    }
+    Deque();
+    ~Deque();
+    Deque(const Deque<T>& other);
+    Deque<T>& operator=(const Deque<T>& other);
+    const T& front() const;
+    const T& back() const;
+    void push_front(const T& value);
+    void push_back(const T& value);
+    void pop_front();
+    void pop_back();
+    bool empty() const;
+    void clear();
 
   private:
     struct Node {
@@ -111,3 +32,112 @@ class Deque {
     Node* last;
     std::size_t sz;
 };
+
+template<typename T>
+Deque<T>::Deque() : first(nullptr), last(nullptr),sz(0) {}
+
+template<typename T>
+Deque<T>::~Deque() {
+    clear();
+}
+
+template<typename T>
+Deque<T>::Deque(const Deque<T>& other) : first(nullptr), last(nullptr), sz(0) {
+    Node* cur = other.first;
+    while (cur != nullptr) {
+        push_back(cur->val);
+        cur = cur->next;
+    }
+}
+
+template<typename T>
+Deque<T>& Deque<T>::operator=(const Deque<T>& other) {
+    if (this == &other) return *this;
+
+    clear();
+    Node* cur = other.first;
+    while (cur != nullptr) {
+        push_back(cur->val);
+        cur = cur->next;
+    }
+
+    return *this;
+}
+
+template<typename T>
+const T& Deque<T>::front() const {
+    if (empty()) {
+        throw std::out_of_range("Deque empty\n");
+    }
+    return first->val;
+}
+
+template<typename T>
+const T& Deque<T>::back() const {
+    if (empty()) {
+        throw std::out_of_range("Deque empty\n");
+    }
+    return last->val;
+}
+
+template<typename T>
+void Deque<T>::push_front(const T& value) {
+    Node* newNode = new Node(value);
+    if (first == nullptr) {
+        first = newNode;
+        last = newNode;
+    } else {
+        first->prev = newNode;
+        newNode->next = first;
+        first = newNode;
+    }
+    sz++;
+}
+
+template<typename T>
+void Deque<T>::push_back(const T& value) {
+    Node* newNode = new Node(value);
+    if (first == nullptr) {
+        first = newNode;
+        last = newNode;
+    } else {
+        newNode->prev = last;
+        last->next = newNode;
+        last = last->next;
+    }
+    sz++;
+}
+
+template<typename T>
+void Deque<T>::pop_front() {
+    if (empty()) return;
+    Node* oldFirst = first;
+    first = first->next;
+    if (first != nullptr) first->prev = nullptr;
+    delete oldFirst;
+    sz--;
+    if (sz == 0) last = nullptr;
+}
+
+template<typename T>
+void Deque<T>::pop_back() {
+    if (empty()) return;
+    Node* oldLast = last;
+    last = last->prev;
+    if (last != nullptr) last->next = nullptr;
+    delete oldLast;
+    sz--;
+    if (sz == 0) first = nullptr;
+}
+
+template<typename T>
+bool Deque<T>::empty() const {
+    return (sz == 0);
+}
+
+template<typename T>
+void Deque<T>::clear() {
+    while (!empty()) pop_front();
+}
+
+#endif // DEQUE_HPP
--- Queue.hpp
+++ Queue.hpp
@@ -1,80 +1,103 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
 #include <cstddef>
 #include <stdexcept>
 
 template<typename T>
 class Queue {
   public:
-    Queue() : first(nullptr), last(nullptr),sz(0) {}
-
-    ~Queue() {
-        clear();
-    }
-
-    Queue(const Queue& other) : first(nullptr), last(nullptr), sz(0) {
-        Node* cur = other.first;
-        while (cur != nullptr) {
-            push(cur->val);
-            cur = cur->next;
-        }
-    }
-
-    Queue& operator=(const Queue& other) {
-        if (this == &other) return *this;
-
-        clear();
-        Node* cur = other.first;
-        while (cur != nullptr) {
-            push(cur->val);
-            cur = cur->next;
-        }
-
-        return *this;
-    }
-
-    const T& front() const {
-        if (empty()) {
-            throw std::out_of_range("Queue empty\n");
-        }
-        return first->val;
-    }
-
-    void push(const T& value) {
-        Node* newNode = new Node(value);
-        if (first == nullptr) {
-            first = newNode;
-            last = newNode;
-        } else {
-            last->next = newNode;
-            last = last->next;
-        }
-        sz++;
-    }
-
-    void pop() {
-        if (empty()) return;
-        Node* oldFirst = first;
-        first = first->next;
-        delete oldFirst;
-        sz--;
-        if (sz == 0) last = nullptr;
-    }
-
-    bool empty() const {
-        return (sz == 0);
-    }
-
-    void clear() {
-        while (!empty()) pop();
-    }
-
+    Queue();
+    ~Queue();
+    Queue(const Queue<T>& other);
+    Queue<T>& operator=(const Queue<T>& other);
+    const T& front() const;
+    void push(const T& value);
+    void pop();
+    bool empty() const;
+    void clear();
+    
   private:
     struct Node {
         T val;
         Node* next;
-
         Node(const T&v) : val(v), next(nullptr) {}
     };
     Node* first;
     Node* last;
     std::size_t sz;
 };
+
+template<typename T>
+Queue<T>::Queue() : first(nullptr), last(nullptr),sz(0) {}
+
+template<typename T>
+Queue<T>::~Queue() {
+    clear();
+}
+
+template<typename T>
+Queue<T>::Queue(const Queue<T>& other) : first(nullptr), last(nullptr), sz(0) {
+    Node* cur = other.first;
+    while (cur != nullptr) {
+        push(cur->val);
+        cur = cur->next;
+    }
+}
+
+template<typename T>
+Queue<T>& Queue<T>::operator=(const Queue<T>& other) {
+    if (this == &other) return *this;
+
+    clear();
+    Node* cur = other.first;
+    while (cur != nullptr) {
+        push(cur->val);
+        cur = cur->next;
+    }
+
+    return *this;
+}
+
+template<typename T>
+const T& Queue<T>::front() const {
+    if (empty()) {
+        throw std::out_of_range("Queue empty\n");
+    }
+    return first->val;
+}
+
+template<typename T>
+void Queue<T>::push(const T& value) {
+    Node* newNode = new Node(value);
+    if (first == nullptr) {
+        first = newNode;
+        last = newNode;
+    } else {
+        last->next = newNode;
+        last = last->next;
+    }
+    sz++;
+}
+
+template<typename T>
+void Queue<T>::pop() {
+    if (empty()) return;
+    Node* oldFirst = first;
+    first = first->next;
+    delete oldFirst;
+    sz--;
+    if (sz == 0) last = nullptr;
+}
+
+template<typename T>
+bool Queue<T>::empty() const {
+    return (sz == 0);
+}
+
+template<typename T>
+void Queue<T>::clear() {
+    while (!empty()) pop();
+}
+
+#endif // QUEUE_HPP
