f6628a38a768b5012f631ab2e6474ad58186586f
ehabda05 <habda.escander@gmail.com>
2026-05-31 19:26:58 -0400

    Fixed Binary Search Tree and Cleaned Format

--- BinarySearchTree.hpp
+++ BinarySearchTree.hpp
@@ -1,87 +1,190 @@
+#ifndef BINARYSEARCHTREE_HPP
+#define BINARYSEARCHTREE_HPP
+
 #include <iostream>
-#include <queue>
+#include <cstddef>
+#include "Queue.hpp"
+#include "Vector.hpp"
 
-struct Node {
-    int val;
-    Node* left;
-    Node* right;
-
-    Node(int v) : val(v), left(nullptr), right(nullptr) {}
-};
-
+template<typename T>
 class BinarySearchTree {
   public:
-    template<typename T>
-    Node* BuildTree(const T& data) {
-        if (data.empty()) return nullptr;
-        std::size_t n = data.size();
-        Node* root = new Node(data[0]);
-        for (std::size_t i = 1; i < n; i++) {
-            Node* cur = root;
-            while (cur != nullptr) {
-                if (data[i] < cur->val) {
-                    if (cur->left != nullptr) {
-                        cur = cur->left;
-                        continue;
-                    }
-                    Node* newNode = new Node(data[i]);
-                    cur->left = newNode;
-                    break;
-                }
-                else if (data[i] > cur->val) {
-                    if (cur->right != nullptr) {
-                        cur = cur->right;
-                        continue;
-                    }
-                    Node* newNode = new Node(data[i]);
-                    cur->right = newNode;
-                    break;
+    struct Node {
+        T val;
+        Node* left;
+        Node* right;
 
-                }
-                else {
-                    std::cout << "Duplicate of " << data[i] << " detected\n";
-                    break;
-                }
-            }
-        }
-        return root;
-    }
-
-    void PreOrder(Node* root) {
-        if (root == nullptr) return;
-        std::cout << root->val << "\t";
-        PreOrder(root->left);
-        PreOrder(root->right);
-    }
-
-    void InOrder(Node* root) {
-        if (root == nullptr) return;
-        InOrder(root->left);
-        std::cout << root->val << "\t";
-        InOrder(root->right);
-    }
-
-    void PostOrder(Node* root) {
-        if (root == nullptr) return;
-        PostOrder(root->left);
-        PostOrder(root->right);
-        std::cout << root->val << "\t";
-    }
-
-    void LevelOrder(Node* root) {
-        if (root == nullptr) return;
-        std::queue<Node*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            std::size_t n = q.size();
-            for (std::size_t i = 0; i < n; i++) {
-                Node* cur = q.front(); q.pop();
-                std::cout << cur->val << "\t";
-                if (cur->left) q.push(cur->left);
-                if (cur->right) q.push(cur->right);
-            }
-            std::cout << "\n";
-        }
-    }
+        Node(const T& v) : val(v), left(nullptr), right(nullptr) {}
+    };
+    BinarySearchTree();
+    ~BinarySearchTree();
+    BinarySearchTree(const BinarySearchTree<T>& other);
+    BinarySearchTree<T>& operator=(const BinarySearchTree<T>& other);
+    void BuildTree(const Vector<T>& data);
+    void insert(const T& value);
+    void clear();
+    Node* getRoot() const;
+    void PreOrder(Node* node);
+    void InOrder(Node* node);
+    void PostOrder(Node* node);
+    void LevelOrder(Node* node);
+  private:
+    Node* root;
 };
+
+
+template<typename T>
+BinarySearchTree<T>::BinarySearchTree() : root(nullptr) {}
+
+
+template<typename T>
+BinarySearchTree<T>::~BinarySearchTree() {
+    clear();
+}
+
+template<typename T>
+BinarySearchTree<T>::BinarySearchTree(const BinarySearchTree<T>& other) : root(nullptr) {
+    if (other.root == nullptr) return;
+    Queue<Node*> q;
+    q.push(other.root);
+
+    while (!q.empty()) {
+        Node* cur = q.front(); q.pop();
+        if (cur->left) q.push(cur->left);
+        if (cur->right) q.push(cur->right);
+        insert(cur->val);
+    }
+}
+
+
+template<typename T>
+BinarySearchTree<T>& BinarySearchTree<T>::operator=(const BinarySearchTree<T>& other) {
+    if (this == &other) return *this;
+    clear();
+    if (other.root == nullptr) return *this;
+    Queue<Node*> q;
+    q.push(other.root);
+
+    while (!q.empty()) {
+        Node* cur = q.front(); q.pop();
+        if (cur->left) q.push(cur->left);
+        if (cur->right) q.push(cur->right);
+        insert(cur->val);
+    }
+    return *this;
+}
+    
+template<typename T>
+void BinarySearchTree<T>::BuildTree(const Vector<T>& data) {
+    if (data.empty()) return;
+    std::size_t n = data.size();
+    for (std::size_t i = 0; i < n; i++) {
+        insert(data[i]);
+    }
+}
+
+
+template<typename T>
+void BinarySearchTree<T>::insert(const T& value) {
+    Node* newNode = new Node(value);
+    if (root == nullptr) {
+        root = newNode;
+        return;
+    }
+    Node* cur = root;
+    while (cur != nullptr) {
+        if (value < cur->val) {
+            if (cur->left != nullptr) {
+                cur = cur->left;
+                continue;
+            }
+            cur->left = newNode;
+            return;
+        }
+        else if (value > cur->val) {
+            if (cur->right != nullptr) {
+                cur = cur->right;
+                continue;
+            }
+            cur->right = newNode;
+            return;
+
+        }
+        else {
+            std::cout << "Duplicate of " << value << " detected\n";
+            delete newNode;
+            return;
+        }
+    }
+}
+
+
+template<typename T>
+void BinarySearchTree<T>::clear() {
+    if (root == nullptr) return;
+    Queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        Node* cur = q.front(); q.pop();
+        if (cur->left) q.push(cur->left);
+        if (cur->right) q.push(cur->right);
+        delete cur;
+    }
+
+    root = nullptr;
+}
+
+
+template<typename T>
+typename BinarySearchTree<T>::Node* BinarySearchTree<T>::getRoot() const {
+    return root;
+}
+
+
+template<typename T>
+void BinarySearchTree<T>::PreOrder(Node* node) {
+    if (node == nullptr) return;
+    std::cout << node->val << "\t";
+    PreOrder(node->left);
+    PreOrder(node->right);
+}
+
+
+template<typename T>
+void BinarySearchTree<T>::InOrder(Node* node) {
+    if (node == nullptr) return;
+    InOrder(node->left);
+    std::cout << node->val << "\t";
+    InOrder(node->right);
+}
+
+
+template<typename T>
+void BinarySearchTree<T>::PostOrder(Node* node) {
+    if (node == nullptr) return;
+    PostOrder(node->left);
+    PostOrder(node->right);
+    std::cout << node->val << "\t";
+}
+
+
+template<typename T>
+void BinarySearchTree<T>::LevelOrder(Node* node) {
+    if (node == nullptr) return;
+    Queue<Node*> q;
+    q.push(node);
+
+    while (!q.empty()) {
+        std::size_t n = q.size();
+        for (std::size_t i = 0; i < n; i++) {
+            Node* cur = q.front(); q.pop();
+            std::cout << cur->val << "\t";
+            if (cur->left) q.push(cur->left);
+            if (cur->right) q.push(cur->right);
+        }
+        std::cout << "\n";
+    }
+}
+
+#endif // BINARYSEARCHTREE_HPP
