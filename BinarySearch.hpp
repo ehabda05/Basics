6c3b0b67840bf2cf0cf55d2a44f9c219998e0259
ehabda05 <habda.escander@gmail.com>
2026-05-31 18:08:13 -0400

    Cleaner Binary Search Format

--- BinarySearch.hpp
+++ BinarySearch.hpp
@@ -1,19 +1,27 @@
+#ifndef BINARYSEARCH_HPP
+#define BINARYSEARCH_HPP
+
 #include <cstddef>
 
 class BinarySearch {
   public:
     template<typename T, typename U>
-    bool search(const T& data, const U& key) {
-        std::size_t l = 0;
-        std::size_t r = data.size();
-
-        while (l < r) {
-            std::size_t m = l + ((r-l)/2);
-            if (data[m] == key) return true;
-            else if (data[m] < key) l = m+1;
-            else r = m;
-        }
-
-        return false;
-    }
+    bool search(const T& data, const U& key);
 };
+
+template<typename T, typename U>
+bool BinarySearch::search(const T& data, const U& key) {
+    std::size_t l = 0;
+    std::size_t r = data.size();
+
+    while (l < r) {
+        std::size_t m = l + ((r-l)/2);
+        if (data[m] == key) return true;
+        else if (data[m] < key) l = m+1;
+        else r = m;
+    }
+
+    return false;
+}
+
+#endif // BINARYSEARCH_HPP
