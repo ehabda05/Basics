050d443a0d48a96cb6f012a5fa6826bfd593824e
ehabda05 <habda.escander@gmail.com>
2026-06-06 17:00:51 -0400

    MinHeap and MaxHeap Header Files Finished

--- MaxHeap.hpp
+++ MaxHeap.hpp
@@ -26,4 +26,102 @@ class MaxHeap{
     void heapifyDown(std::size_t index);    
 };
 
+template<typename T> 
+MaxHeap<T>::MaxHeap() {}
+
+template<typename T>
+MaxHeap<T>::~MaxHeap() {}
+
+template<typename T>
+MaxHeap<T>::MaxHeap(const MaxHeap<T>& other) : data(other.data) {}
+
+template<typename T>
+MaxHeap<T>& MaxHeap<T>::operator=(const MaxHeap<T>& other) {
+  if (this == &other) return *this;
+  data = other.data;
+  return *this;
+}
+
+template<typename T>
+void MaxHeap<T>::push(const T& value) {
+  data.push_back(value);
+  heapifyUp(data.size()-1);
+}
+
+template<typename T>
+void MaxHeap<T>::pop() {
+  if (empty()) {
+    throw std::out_of_range("Heap Empty\n");
+  }
+
+  data[0] = data[size()-1];
+  data.pop_back();
+  if (!empty()) heapifyDown(0);
+}
+
+template<typename T>
+T& MaxHeap<T>::top() {
+  if (empty()) {
+    throw std::out_of_range("Heap Empty\n");
+  }
+  return data[0];
+}
+
+template<typename T>
+const T& MaxHeap<T>::top() const {
+  if (empty()) {
+    throw std::out_of_range("Heap Empty\n");
+  }
+  return data[0];
+}
+
+template<typename T>
+bool MaxHeap<T>::empty() const {
+  return data.empty();
+}
+
+template<typename T>
+std::size_t MaxHeap<T>::size() const {
+  return data.size();
+}
+
+template<typename T>
+void MaxHeap<T>::clear() {
+  data.clear();
+}
+
+template<typename T>
+void MaxHeap<T>::heapifyUp(std::size_t index) {
+  while (index > 0) {
+        std::size_t parent = (index - 1) / 2;
+
+        if (data[parent] >= data[index]) break;
+        T temp = data[parent];
+        data[parent] = data[index];
+        data[index] = temp;
+
+        index = parent;
+    }
+}
+
+template<typename T>
+void MaxHeap<T>::heapifyDown(std::size_t index) {
+  std::size_t n = size();
+  while (index < n) {
+        std::size_t left = index*2 + 1;
+        std::size_t right = index*2 + 2;
+        std::size_t largest = index;
+        
+        if (left < n && data[left] > data[largest]) largest = left;
+        if (right < n && data[right] > data[largest]) largest = right;
+        if (largest == index) break;
+
+        T temp = data[index];
+        data[index] = data[largest];
+        data[largest] = temp;
+
+        index = largest;
+    }
+} 
+
 #endif // MAXHEAP_HPP
--- MinHeap.hpp
+++ MinHeap.hpp
@@ -26,4 +26,102 @@ class MinHeap{
     void heapifyDown(std::size_t index);    
 };
 
+template<typename T> 
+MinHeap<T>::MinHeap() {}
+
+template<typename T>
+MinHeap<T>::~MinHeap() {}
+
+template<typename T>
+MinHeap<T>::MinHeap(const MinHeap<T>& other) : data(other.data) {}
+
+template<typename T>
+MinHeap<T>& MinHeap<T>::operator=(const MinHeap<T>& other) {
+  if (this == &other) return *this;
+  data = other.data;
+  return *this;
+}
+
+template<typename T>
+void MinHeap<T>::push(const T& value) {
+  data.push_back(value);
+  heapifyUp(data.size()-1);
+}
+
+template<typename T>
+void MinHeap<T>::pop() {
+  if (empty()) {
+    throw std::out_of_range("Heap Empty\n");
+  }
+
+  data[0] = data[size()-1];
+  data.pop_back();
+  if (!empty()) heapifyDown(0);
+}
+
+template<typename T>
+T& MinHeap<T>::top() {
+  if (empty()) {
+    throw std::out_of_range("Heap Empty\n");
+  }
+  return data[0];
+}
+
+template<typename T>
+const T& MinHeap<T>::top() const {
+  if (empty()) {
+    throw std::out_of_range("Heap Empty\n");
+  }
+  return data[0];
+}
+
+template<typename T>
+bool MinHeap<T>::empty() const {
+  return data.empty();
+}
+
+template<typename T>
+std::size_t MinHeap<T>::size() const {
+  return data.size();
+}
+
+template<typename T>
+void MinHeap<T>::clear() {
+  data.clear();
+}
+
+template<typename T>
+void MinHeap<T>::heapifyUp(std::size_t index) {
+  while (index > 0) {
+        std::size_t parent = (index - 1) / 2;
+
+        if (data[parent] <= data[index]) break;
+        T temp = data[parent];
+        data[parent] = data[index];
+        data[index] = temp;
+
+        index = parent;
+    }
+}
+
+template<typename T>
+void MinHeap<T>::heapifyDown(std::size_t index) {
+  std::size_t n = size();
+  while (index < n) {
+        std::size_t left = index*2 + 1;
+        std::size_t right = index*2 + 2;
+        std::size_t smallest = index;
+        
+        if (left < n && data[left] < data[smallest ]) smallest = left;
+        if (right < n && data[right] < data[smallest ]) smallest = right;
+        if (smallest == index) break;
+
+        T temp = data[index];
+        data[index] = data[smallest];
+        data[smallest ] = temp;
+
+        index = smallest;
+    }
+} 
+
 #endif // MINHEAP_HPP
